2cafaf1c6700173882198c645b8a8349db9e9fc2
lscastr0 <94759494+lscastr0@users.noreply.github.com>
2022-01-07 10:44:16 -0300

    ex008 added

---
+++ ex008/assets/js/script.js
@@ -0,0 +1,36 @@
+function hora(){
+    const hora = new Date();
+    
+    return hora.getHours();
+}
+
+function message(){
+    const hr = hora();
+    const msg = document.getElementById('msg');
+
+    return msg.innerHTML = `Agora são ${hr} hora`;
+}
+
+function image(){
+    const hr = hora();
+    const img = document.getElementById('image');
+
+    if(hr > 0 && hr < 13){
+        img.src = 'assets/img/morning.png';
+    }
+    else if(hr < 18){
+        img.src = 'assets/img/afternoon.png';
+    }
+    else{
+        img.src = "assets/img/night.png"; 
+    }
+
+}
+
+function run(){
+    message();
+    image();
+}
+
+const body = document.querySelector('body');
+body.addEventListener('onload', run());
